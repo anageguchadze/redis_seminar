e52766fcecc9d136b4a2b9f64adac45159b00f65
Ana <anageguchadze22@gmail.com>
2025-05-05 12:53:55 +0400

    Add serializers

---
+++ redis_app/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Category, Animal
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+class AnimalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
