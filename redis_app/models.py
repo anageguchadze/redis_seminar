0a3bf8118ab9d3e975c78b63d7db6a8a5ceda85d
Ana <anageguchadze22@gmail.com>
2025-05-05 12:50:44 +0400

    Add models

--- redis_app/models.py
+++ redis_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+    
+class Animal(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='animals')
+    scientific_name = models.CharField(max_length=100)
+    description = models.TextField()
+    habitat = models.CharField(max_length=255)
+    diet = models.CharField(max_length=255)
+    status = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
--- redis_pro/settings.py
+++ redis_pro/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'django_filters',
+    'redis_app',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
