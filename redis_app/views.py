882e1f405b800c77e4f1be78277a72dd65b17ae0
Ana <anageguchadze22@gmail.com>
2025-05-05 13:41:21 +0400

    Add filters

--- redis_app/views.py
+++ redis_app/views.py
@@ -1,6 +1,9 @@
 from rest_framework import generics
 from .models import Category, Animal
 from .serializers import CategorySerializer, AnimalSerializer
+from .filters import AnimalFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 
 
 class CategoryList(generics.ListCreateAPIView):
@@ -16,6 +19,9 @@ class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
 class AnimalList(generics.ListCreateAPIView):
     queryset = Animal.objects.all()
     serializer_class = AnimalSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_class = AnimalFilter
+    search_fields = ['name', 'diet']
 
 class AnimalDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Animal.objects.all()
