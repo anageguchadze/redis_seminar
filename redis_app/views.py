99335a99f581872e8c8b4ce3d2fe5d48cfb6b03d
Ana <anageguchadze22@gmail.com>
2025-05-06 13:12:31 +0400

    Add cache function

--- redis_app/views.py
+++ redis_app/views.py
@@ -4,6 +4,9 @@ from .serializers import CategorySerializer, AnimalSerializer
 from .filters import AnimalFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
+from django.core.cache import cache
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
 class CategoryList(generics.ListCreateAPIView):
@@ -19,9 +22,19 @@ class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
 class AnimalList(generics.ListCreateAPIView):
     queryset = Animal.objects.all()
     serializer_class = AnimalSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter]
-    filterset_class = AnimalFilter
-    search_fields = ['name', 'diet']
+    # filter_backends = [DjangoFilterBackend, SearchFilter]
+    # filterset_class = AnimalFilter
+    # search_fields = ['name', 'diet']
+    def list(self, request, *args, **kwargs):
+        cache_key = 'animal_list'
+        cache_time = 60 * 5
+        data = cache.get(cache_key)
+        if not data:
+            animals = Animal.objects.all()
+            data = AnimalSerializer(animals, many=True).data
+            cache.set(cache_key, data, cache_time)
+        return Response(data)
+
 
 class AnimalDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Animal.objects.all()
