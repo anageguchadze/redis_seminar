042f9d6d11bf801a9f530d03aa681954247ef3de
Ana <anageguchadze22@gmail.com>
2025-05-06 12:45:36 +0400

    Add redis cache

--- redis_pro/settings.py
+++ redis_pro/settings.py
@@ -129,3 +129,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 3
 }
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+    }
+}
